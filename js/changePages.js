99887cab680df9b40b48d8e39f00fa7a94f49f4e
Pulkio <guillaume.cobat@gmx.fr>
2022-11-28 18:23:56 +0100

    ajout page contact

--- js/changePages.js
+++ js/changePages.js
@@ -213,3 +213,19 @@ function redirectPages(clicked_id) {
 
     }
 }
+
+
+function CopyToClipboard(containerid) {
+    if (document.selection) {
+      var range = document.body.createTextRange();
+      range.moveToElementText(document.getElementById(containerid));
+      range.select().createTextRange();
+      document.execCommand("copy");
+    } else if (window.getSelection) {
+      var range = document.createRange();
+      range.selectNode(document.getElementById(containerid));
+      window.getSelection().addRange(range);
+      document.execCommand("copy");
+      alert("Text has been copied, now paste in the text-area")
+    }
+  }
